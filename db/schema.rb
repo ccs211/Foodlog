75ed30f5cfdf95b6a48c050054c142b44137e59f
Alejandro Vargas Madrid <alejandro.vargasmadrid@gmail.com>
2020-10-29 19:01:10 +0100

    PostgresQL changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_143830) do
+ActiveRecord::Schema.define(version: 2020_10_22_143830) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "entries", force: :cascade do |t|
     t.string "meal_type"
